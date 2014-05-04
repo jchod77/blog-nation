67c9a5177d995ffc9ca07b87ba08b2fc69679ef1
Jason Chodera <jason.chodera@outlook.com>
2014-05-03 20:31:22 -0500

    user and post routes completed

--- source/blog-nation/app/controllers/index.rb
+++ source/blog-nation/app/controllers/index.rb
@@ -1,11 +1,29 @@
-get '/' do
+enable :sessions
+
+#SECURITY FILTER
+#-----------------------------------------------------
+before '/users/secure/*' do
   if !session[:id]
-    @id = session[:id]
-    @posts = Post.find_all_by_user_id(@id)
+    redirect '/'
+  end
+end
+
+get '/' do
+  if session[:id]
+    @user = User.find_by_id(session[:id])
+    @posts = Post.find_all_by_user_id(session[:id])
   else
-    @posts = Post.all
+    # @posts = Post.all
   end
 
   @tags = Tag.all
   erb :index
 end
+
+get '/users/all' do
+@users = User.all
+@tags = Tag.all
+erb :all_users
+end
+
+
--- source/blog-nation/app/controllers/posts.rb
+++ source/blog-nation/app/controllers/posts.rb
@@ -1,5 +1,72 @@
-get '/posts/:id' do
+enable :sessions
+
+#SECURITY FILTER
+#-----------------------------------------------------
+before '/users/secure/*' do
+  if !session[:id]
+    redirect '/'
+  end
+end
+
+get '/posts/all' do
+  @posts = Post.all
   @tags = Tag.all
-  @posts = Post.find_all_by_user_id(params[:id])
+  @user = User.find_by_id(session[:id])
+  erb :all_posts
+end
+
+get '/posts/:id' do
+  @post = Post.find(params[:id])
+  @tags = Tag.all
+
+  erb :single_post
+end
+
+get '/users/secure/posts/create' do
+  @tags = Tag.all
+
+  erb :create_post
+
+end
+
+post '/users/secure/posts/create' do
+  @post = Post.new(params[:post])
+  @post.save
+  redirect '/posts/:id'
+end
+
+get '/users/secure/posts/all/:user_id' do
+  @tags = Tag.all
+  @posts = Post.find_all_by_user_id(params[:user_id])
   erb :profile
 end
+
+get '/posts/edit/:id' do
+  @post = Post.find(params[:id])
+  @tags = Tag.all
+
+  erb :edit_post
+end
+post '/posts/edit/:id' do
+  @post = Post.find(params[:id])
+  @post.update_attributes(params[:post])
+
+  redirect '/'
+
+end
+
+get '/posts/delete/:id' do
+   @post = Post.find(params[:id])
+   @tags = Tag.all
+   erb :delete_post
+end
+
+post '/posts/delete/:id' do
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect '/'
+
+end
+
+
+
